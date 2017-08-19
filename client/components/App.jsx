1bb87d3ce11566a18cc801d9c924d144679168bf
shatadipa <shatadipa1986@gmail.com>
2017-08-19 15:57:39 -0700

    Test a basic React app

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+        <div style="color:red">
+          <h1>musik</h1>
+        </div>
+      );
+  }
+}
