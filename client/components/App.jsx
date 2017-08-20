b3e0290382cdd38d8cd2cc3d12e7327803fb5040
shatadipa <shatadipa1986@gmail.com>
2017-08-20 03:28:53 -0700

    Adds CSS Module Support

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import styles from './app.css';
 
 export default class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
+      <div className={[].join(' ')} id={styles.logo}>
+          <h1>musik</h1>
+      </div>
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,20 @@ module.exports = {
     filename: 'index_bundle.js'
   },
   module: {
-    loaders: [{
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'          
+        }
+      },
+      {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
