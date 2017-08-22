cceaa67cff2e600f6e4d31042fba5e8e2ad1a76d
shatadipa <shatadipa1986@gmail.com>
2017-08-22 15:06:32 -0700

    Update Song row to use flex

--- client/components/song/Song.jsx
+++ client/components/song/Song.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './song.css';
 import AlbumArt from '../albumArt/AlbumArt.jsx';
+import SongNumber from '../songNumber/SongNumber.jsx';
 
 export default class Song extends React.Component {
   render() {
     return (
       <li className={styles.song}>
-        <div>#1</div>
+        <SongNumber />
         <AlbumArt source={this.props.song.albumart}/>
         <div>{this.props.song.title}</div>
         <div>{this.props.song.duration}</div>
-        <div></div>
+        <div>...</div>
       </li>
     );
   }
