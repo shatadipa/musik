ec5587dfb8c7f917c71de34bae14e0568ec2c8b7
shatadipa <shatadipa1986@gmail.com>
2017-08-22 15:06:48 -0700

    Adds SongNumber component

---
+++ client/components/songNumber/SongNumber.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './songNumber.css';
+
+export default class SongNumber extends React.Component {
+    render () {
+        return (
+            <div>Number</div>
+        );
+    }
+}
